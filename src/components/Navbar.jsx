3d62050871fe553d4a563ff6c3d27716b5555c00
Aqib-dev05 <aqibgithub@gmail.com>
2025-05-30 18:24:14 +0500

    hello

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Text,X } from "lucide-react";
+import { Text, X } from "lucide-react";
 import { useState } from "react";
 
 export default function Navbar() {
@@ -45,7 +45,7 @@ export default function Navbar() {
         <div className="fixed top-0 left-0 w-full h-full bg-[rgba(26,47,100,0.68)]  text-white  z-50">
           <div className=" h-full p-5">
             <button onClick={handleClose} className="text-black fixed z-50 top-7 right-10 text-2xl">
-              <X color="white" size={48}  />
+              <X color="white" size={48} />
             </button>
             <div className="mt-26 items-center flex flex-col space-y-5 font-medium text-[24px]">
               <a href="#" className="block hover:opacity-100 opacity-90 ">
