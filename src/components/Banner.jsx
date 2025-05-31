72eacf1a26b1d8bdbdfff97ec7b54c37ecaf12bf
Aqib-dev05 <aqibgithub@gmail.com>
2025-05-31 11:49:44 +0500

    images are added to repository

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,13 @@ export default function Banner() {
     return (
         <>
            <section className="h-[90vh] bg-linear-to-r from-[#1b275d] to-[#0f172a] ">
-            <div className="left"></div>
+            <div className="left">
+                <p>Hi,I'm</p>
+                <h1>Aqib ALi A Fronted React Dev<span>.</span> </h1>
+            </div>
+            <div className="right">
+                <img src="" alt="" />
+            </div>
            </section>
         </>
     )
