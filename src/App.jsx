8127f7f7993e7d7ec8d14bb9cb88f970df84f80c
Aqib-dev05 <aqibgithub@gmail.com>
2025-05-31 09:21:27 +0500

    First page is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import FirstMainPage from "./pages/FirstMainPage";
 
 function App() {
   return <>
-    <Navbar/>
+    <FirstMainPage/>
    
   
   </>;
