d7184e73a44d2f7b61b77b352af4fa2207c31d41
Aqib-dev05 <aqibgithub@gmail.com>
2025-05-31 16:18:33 +0500

    .

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,18 @@
-import ProfilePic from '../assets/Avatar.png';
+import ProfilePic from '../assets/Avatar2.png';
 import Button from "./Button"
 
 export default function Banner() {
     return (
         <>
-           <section className="h-[90vh] bg-linear-to-r from-[#1b275d] to-[#0f172a] w-[100vw] flex items-center justify-center ">
-            <div className="h-[100%] w-[50%] text-white bg-red-800">
-                <p>Hi,I'm</p>
-                <h1>Aqib ALi A Fronted React Dev<span>.</span> </h1>
-                <Button text={"My Projects"} />
+           <section className="min-h-[90vh] py-3 bg-linear-to-r from-[#16213b] to-[#0f172a] w-[100vw] flex max-md:flex-col-reverse items-center justify-center md:gap-52 max-md:gap-20 max-md:pl-2">
+            <div className="w-xl max-md:pl-28 max-sm:pl-16 max-sm:w-[100vw]   pl-22 text-white ">
+                <p className='text-xl   font-bold text-[#a3a3a3] '>Hi,I'm</p>
+                <h1 className='text-6xl max-sm:text-[40px] max-md:text-[50px]   w-[95%] leading-[1.25] tracking-[2px]  pb-4 font-bold' >Aqib Ali <br /> A Fronted React Dev<span className='text-[#00bcd4]'>.</span> </h1>
+                <Button text={"My Recent Projects"} />
             </div>
-            <div className=" h-[100%] w-[50%] bg-green-500">
-                <div>
-                <img src={ProfilePic} alt="profile" />
+            <div className="w-xl  max-md:ml-0 max-md:mt-10 flex items-center justify-center">   
+                <div className='size-[360px] max-sm:size-[230px] max-md:size-[300px] max-md:border-0    border-[#00bcd4] border-l-8 border-b-8 border-t-8  relative '>
+                <img className='scale-[1.15]   object-cover absolute top-[-40px] left-0 ' src={ProfilePic} alt="profile" />
                 </div>
             </div>
            </section>
