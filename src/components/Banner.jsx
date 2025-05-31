d5d31c71d5fd4b27441fa2314c121ef5dab6794a
Aqib-dev05 <aqibgithub@gmail.com>
2025-05-31 16:25:26 +0500

    ..

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ export default function Banner() {
             <section className="min-h-[90vh] py-3 bg-linear-to-r from-[#16213b] to-[#0f172a] w-[100vw] flex max-lg:flex-col-reverse items-center justify-center lg:gap-52 max-lg:gap-20 max-lg:pl-2">
                 <div className="w-xl max-lg:pl-28 max-sm:pl-16 max-sm:w-[100vw]   pl-22 text-white ">
                     <p className='text-xl   font-bold text-[#a3a3a3] '>Hi,I'm</p>
-                    <h1 className='text-6xl max-sm:text-[40px] max-lg:text-[50px]   w-[95%] leading-[1.25] tracking-[2px]  pb-4 font-bold' >Aqib Ali <br /> A FrontEnd React Dev<span className='text-[#00bcd4]'>.</span> </h1>
+                    <h1 className='text-6xl max-sm:text-[40px] max-lg:text-[50px]   w-[95%] leading-[1.25] tracking-[2px]   pb-4 font-bold' >Aqib Ali <br /> A FrontEnd React Dev<span className='text-[#00bcd4]'>.</span> </h1>
                     <Button text={"My Recent Projects"} />
                 </div>
                 <div className="w-xl  max-lg:ml-0 max-lg:mt-10 flex items-center justify-center">
